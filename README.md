4d65584477a3ca2812ab12f5e996375594d35e3c
Mitchell spano <MitchSpano@gmail.com>
2022-07-31 22:55:27 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # sfdx-scan-pull-request
+
 Runs sfdx-scanner on a pull request and generates in-line comments with the findings.
+
+![Example](images/sfdx-scan-pull-request.png)
+
+## Inputs
+
+## `severity-threshold`
+
+Throws an error when violations of specific severity (or more severe) are detected.
+
+## `strictly-enforced-rules`
+
+A JSON string which contains the rules which will be strictly enforced regardless of their priority. Enforced rules are identified by their engine, category, and rule name.
+
+<!--
+TODO : Implement additional attributes passed into scan command
+## `category`
+
+Categor(ies) of rules to run.
+
+## `engine`
+
+Engine(s) to run.
+
+ ## `eslint-env`
+
+JSON-formatted string, overrides ESLint's default environment variables.
+
+## `pmdconfig`
+
+Location of PMD rule reference XML file to customize rule selection
+
+## `tsconfig`
+
+Location of tsconfig.json file
+
+## `verbose`
+
+Emit additional command output to stdout -->
+
+## Example usage
+
+```
+name: Static Analysis
+on:
+  pull_request:
+    types: [opened, reopened, edited]
+  workflow_dispatch:
+jobs:
+  analyze:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+      - name: Install SFDX Scanner
+        run: |
+          node_modules/sfdx-cli/bin/run plugins:install @salesforce/sfdx-scanner
+      - name: Run SFDX Scanner - Report findings as comments
+        uses: mitchspano/sfdx-scan-pull-request@v0.1
+        with:
+          severity-threshold: 4
+          strictly-enforced-rules: '[{ "engine": "pmd", "category": "Performance", "rule": "AvoidDebugStatements" }]'
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+### Included Dependencies
+
+This package includes the following version of the required dependencies:
+
+| npm package     | included version |
+| --------------- | ---------------- |
+| @actions/core   | 1.9.0            |
+| @actions/github | 5.0.3            |
+| @octokit/action | 4.0.4            |
+| parse-diff      | 0.9.0            |
+| recursive-copy  | 2.0.14           |
+| sfdx-cli        | 7.161.0          |
+
+ <!-- @salesforce/sfdx-scanner (sfdx-cli plugin) 2.13.7            -->
+
+For more information regarding the inclusion of these static versioned dependencies, see [here](https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github)
---
+++ index.js
@@ -0,0 +1,273 @@
+/*
+   Copyright 2022 Mitch Spano
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+	https://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+const { execSync } = require("child_process");
+const { Octokit } = require("@octokit/action");
+const copy = require("recursive-copy");
+const core = require("@actions/core");
+const fs = require("fs");
+const github = require("@actions/github");
+const parse = require("parse-diff");
+const path = require("path");
+
+const COMMMENT_HEADER = `| Engine | Category | Rule | Severity | Type |
+| --- | --- | --- | --- | --- |`;
+const DIFF_OUTPUT = "diffBetweenCurrentAndParentBranch.txt";
+const ERROR = "Error";
+const FINDINGS_OUTPUT = "sfdx-scanner-findings.json";
+const RIGHT = "RIGHT";
+const TEMP_DIR_NAME = "temporary";
+const TYPES_OF_INTEREST = new Set().add("add").add("delete");
+const WARNING = "Warning";
+
+filePathToChangedLines = {};
+filePathToComments = {};
+findings = [];
+hasHaltingError = false;
+pullRequest = {};
+userInput = {};
+
+/**
+ * @description Collects and verifies the inputs from the action context and metadata
+ * https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#inputs
+ */
+function initialSetup() {
+  // TODO: validate inputs
+  this.userInput = {
+    severityThreshold: core.getInput("severity-threshold"),
+    category: core.getInput("category"),
+    engine: core.getInput("engine"),
+    eslintEnv: core.getInput("eslint-env"),
+    pmdConfig: core.getInput("pmdconfig"),
+    tsConfig: core.getInput("tsconfig"),
+    verbose: core.getInput("verbose"),
+    strictlyEnforcedRules: core.getInput("strictly-enforced-rules"),
+  };
+  this.pullRequest = github.context?.payload?.pull_request;
+}
+
+/**
+ * @description Calculates the diff from the last commit and
+ * populates a map of filePath -> Set of changed line numbers
+ */
+function getDiffSinceLastCommit() {
+  console.log("Getting difference since Last commit...");
+  execSync(
+    `git diff origin/${this.pullRequest?.base?.ref}...origin/${this.pullRequest?.head?.ref} > ${DIFF_OUTPUT}`
+  );
+  var files = parse(fs.readFileSync(DIFF_OUTPUT).toString());
+  files.forEach(function (file) {
+    if (fs.existsSync(file.to)) {
+      let changedLines = new Set();
+      for (let chunk of file.chunks) {
+        for (let change of chunk.changes) {
+          if (TYPES_OF_INTEREST.has(change.type)) {
+            changedLines.add(change.ln);
+          }
+        }
+      }
+      this.filePathToChangedLines[file.to] = changedLines;
+    }
+  });
+}
+
+/**
+ * @description Moves all folders and files which were present in the
+ * git difference to a temporary folder.
+ */
+async function recursivelyMoveFilesToTempFolder() {
+  console.log("Recursively moving all files to the temp folder...");
+  let filesWithChanges = Object.keys(this.filePathToChangedLines);
+  for (let file of filesWithChanges) {
+    await copy(file, path.join(TEMP_DIR_NAME, file), {
+      overwrite: true,
+    }).catch(function (error) {
+      core.setFailed("Copy failed: " + error);
+    });
+  }
+}
+
+/**
+ * @description Uses the sfdx scanner to run static code analysis on
+ * all files within the temporary directory.
+ */
+function performStaticCodeAnalysisOnFilesInDiff() {
+  console.log(
+    "Performing static code analysis on all of the files in the difference..."
+  );
+  // TODO: Implement additional attributes passed into scan command
+  execSync(
+    `node_modules/sfdx-cli/bin/run scanner:run \
+    --format json \
+    --target "${TEMP_DIR_NAME}" \
+    --outfile "${FINDINGS_OUTPUT}"`
+  );
+  let filePath = path.join(process.cwd(), FINDINGS_OUTPUT);
+  this.findings = JSON.parse(fs.readFileSync(filePath));
+  for (let finding of this.findings) {
+    finding.fileName = finding.fileName.replace(
+      path.join(process.cwd(), TEMP_DIR_NAME),
+      process.cwd()
+    );
+  }
+}
+
+/**
+ * @description Parses the findings from the sfdx scanner execution
+ * and determines if any of the findings are for lines which have changed.
+ * If a finding exists and covers a changed line, then translate that finding
+ * object into a comment object.
+ */
+function filterFindingsToDiffScope() {
+  console.log(
+    "Filtering the findings to just the lines which are part of the pull request..."
+  );
+  for (let finding of this.findings) {
+    let filePath = finding.fileName.replace(process.cwd() + "/", "");
+    relevantLines = filePathToChangedLines[filePath];
+    for (let violation of finding.violations) {
+      if (isInChangedLines(violation, relevantLines)) {
+        if (!filePathToComments[filePath]) {
+          filePathToComments[filePath] = [];
+        }
+        filePathToComments[filePath].push(
+          translateViolationToComment(filePath, violation, finding.engine)
+        );
+      }
+    }
+  }
+}
+
+/**
+ * @description Determines if a found violation contains a line
+ * which has changed
+ * @param {Violation} violation Violation from the sfdx scanner
+ * @param {Set<Integer>} relevantLines Lines in the file which have changed
+ * @returns Boolean
+ */
+function isInChangedLines(violation, relevantLines) {
+  if (violation.endLine) {
+    for (
+      let i = parseInt(violation.line);
+      i <= parseInt(violation.endLine);
+      i++
+    ) {
+      if (relevantLines && relevantLines.has(i)) {
+        return true;
+      }
+    }
+  } else if (relevantLines && relevantLines.has(parseInt(violation.line))) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * @description Translates a violation object into a comment
+ * with a formatted body
+ * @param {Violation} violation Violation from the sfdx scanner
+ * @param {String} engine Engine from the sfdx scanner
+ * @returns Comment
+ */
+function translateViolationToComment(filePath, violation, engine) {
+  let type = isHaltingViolation(violation, engine) ? ERROR : WARNING;
+  if (type == ERROR) {
+    hasHaltingError = true;
+  }
+  let endLine = violation.endLine
+    ? parseInt(violation.endLine)
+    : parseInt(violation.line);
+  let startLine = parseInt(violation.line);
+  if (endLine == startLine) {
+    endLine++;
+  }
+  return {
+    commit_id: this.pullRequest?.head?.sha,
+    path: filePath,
+    start_line: startLine,
+    start_side: RIGHT,
+    side: RIGHT,
+    line: endLine,
+    start_line: startLine,
+    body: `${COMMMENT_HEADER}
+| ${engine} | ${violation.category} | ${violation.ruleName} | ${violation.severity} | ${type} |
+
+[${violation.message}](${violation.url})`,
+  };
+}
+
+/**
+ * @description Calculates if a violation will cause halting or not.
+ * @param {Violation} violation Violation from the sfdx scanner
+ * @param {String} engine Engine from the sfdx scanner
+ * @returns Boolean
+ */
+function isHaltingViolation(violation, engine) {
+  if (
+    this.userInput.severityThreshold &&
+    this.userInput.severityThreshold <= violation.severity
+  ) {
+    return true;
+  }
+  if (!this.userInput.strictlyEnforcedRules) {
+    return false;
+  }
+  let violationDetail = {
+    engine: engine,
+    category: violation.category,
+    rule: violation.ruleName,
+  };
+  for (let enforcedRule of JSON.parse(this.userInput.strictlyEnforcedRules)) {
+    if (
+      Object.entries(violationDetail).toString() ===
+      Object.entries(enforcedRule).toString()
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * @description Writes the relevant comments to the GitHub pull request.
+ * Uses the octokit to post the comments to the PR.
+ */
+async function writeComments() {
+  console.log("Writing comments using GitHub REST API...");
+  const octokit = new Octokit();
+  const owner = this.pullRequest?.base?.repo?.owner?.login;
+  const repo = this.pullRequest?.base?.repo?.name;
+  for (let file in this.filePathToComments) {
+    for (let comment of this.filePathToComments[file]) {
+      const method = `POST /repos/${owner}/${repo}/pulls/${this.pullRequest?.number}/comments`;
+      await octokit.request(method, comment);
+    }
+  }
+  if (hasHaltingError === true) {
+    core.setFailed("A halting error has been identified");
+  }
+}
+
+/**
+ * @description Main method - injection point for code execution
+ */
+async function main() {
+  initialSetup();
+  getDiffSinceLastCommit();
+  await recursivelyMoveFilesToTempFolder();
+  performStaticCodeAnalysisOnFilesInDiff();
+  filterFindingsToDiffScope();
+  writeComments();
+}
+
+main();
