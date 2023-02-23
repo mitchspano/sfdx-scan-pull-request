48cceddc5613801322232e12700656b405e2ec03
James Simone <james.simone@salesforce.com>
2023-02-23 08:13:15 -0700

    Modifying sfdxCli.ts to use npx for node_module resolution

--- src/sfdxCli.ts
+++ src/sfdxCli.ts
@@ -51,7 +51,7 @@ type SfdxCommandResult<T> = {
 const cli = async <T>(commandName: string, cliArgs: string[] = []) => {
   let result = null as T;
   try {
-    const cliCommand = `sfdx ${commandName} ${cliArgs.join(" ")}`;
+    const cliCommand = `npx sfdx ${commandName} ${cliArgs.join(" ")}`;
     result = (
       JSON.parse(execSync(cliCommand).toString()) as SfdxCommandResult<T>
     ).result;
